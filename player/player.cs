b3e2f48c2a47f712a4e83a62963a01b2e178b408
jman45-9 <jcope0787@duck.com>
2023-12-03 16:05:32 -0700

    fixed naming conventions

--- player/player.cs
+++ player/player.cs
@@ -5,7 +5,7 @@ public partial class player : CharacterBody2D
 {
 	private AnimatedSprite2D _animatedSprite;
 	private bool _inputReceived;
-	private const float  _linearAcceleration = 1.0001f;//in px per lindelta
+	private const float  _LinearAcceleration = 1.0001f;//in px per lindelta
 	private int _linDelta = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -21,7 +21,7 @@ public partial class player : CharacterBody2D
 		{
 			_animatedSprite.Play("fwd");
 			_linDelta++;
-			float velocity = _linearAcceleration * _linDelta;
+			float velocity = _LinearAcceleration * _linDelta;
 			GD.Print(velocity);
 			Velocity = new Vector2(0,-velocity); //velocity is negiive bc it will go backward otherwise :)
 			_inputReceived = true;
