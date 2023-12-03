36f6ee6079ec5251b07dd1ddf1d59e5c50a3405c
jman45-9 <jcope0787@duck.com>
2023-12-03 13:11:30 -0700

    made rocket accelrate

--- player/player.cs
+++ player/player.cs
@@ -4,6 +4,9 @@ using System;
 public partial class player : CharacterBody2D
 {
 	private AnimatedSprite2D _animatedSprite;
+	private bool _inputReceived;
+    private const float  _linearAcceleration = 1.0001f;//in px per lindelta
+    private int _linDelta = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,14 +16,26 @@ public partial class player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		_inputReceived = false;
 		if (Input.IsActionPressed("boostFwd"))
 		{
 			_animatedSprite.Play("fwd");
-			Velocity = new Vector2(0,-10);
+            _linDelta++;
+            float velocity = _linearAcceleration * _linDelta;
+            GD.Print(velocity);
+			Velocity = new Vector2(0,-velocity); //velocity is negiive bc it will go backward otherwise :)
+			_inputReceived = true;
 		}
-		else
+        else
+       {
+           if(_linDelta > 0)
+               _linDelta--;
+       }
+
+
+		if(!_inputReceived)
 			_animatedSprite.Play("default");
+
 		MoveAndSlide();
 	}
 }
-
