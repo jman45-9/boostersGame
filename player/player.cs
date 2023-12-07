5b5e0b0de5651f37e6a3a11155e848eb031f3947
jman45-9 <jcope0787@duck.com>
2023-12-06 20:46:06 -0700

    added ability to turn - need to add deceleration

--- player/player.cs
+++ player/player.cs
@@ -3,16 +3,16 @@ using System;
 
 public partial class player : CharacterBody2D
 {
-    private const int LEFT = -1;
-    private const int RIGHT = 1;
+	private const int LEFT = -1;
+	private const int RIGHT = 1;
 
 	private AnimatedSprite2D _animatedSprite;
 	private bool _inputReceived;
 	private const float  _LinearAcceleration = 1.0001f;//in px per frame 
 	private int _linDelta = 0;
 
-    private const float _AngularAccleration = 0.001f;
-    private int _angDelta = 0;
+	private const float _AngularAccleration = 0.001f;
+	private int _angDelta = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -22,31 +22,31 @@ public partial class player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        GD.Print(Math.Abs(Rotation));
 		_inputReceived = false;
 		if (Input.IsActionPressed("boostFwd"))
 		{
 			_animatedSprite.Play("fwd");
 			_linDelta++;
 			float velocity = _LinearAcceleration * _linDelta;
-			GD.Print(velocity);
-			Velocity = new Vector2(0,-velocity); //velocity is negiive bc it will go backward otherwise :)
+			Velocity = new Vector2((float)(velocity * Math.Sin(Rotation)),(float)(-velocity * Math.Cos(Rotation))); //velocity is negiive bc it will go backward otherwise :)
 			_inputReceived = true;
 		}
 
-        if (Input.IsActionPressed("turnL"))
-        {
-            _animatedSprite.Play("turnL");
-            _angDelta--;
-            _inputReceived = true;
-        }
+		if (Input.IsActionPressed("turnL"))
+		{
+			_animatedSprite.Play("turnL");
+			_angDelta--;
+			_inputReceived = true;
+		}
 
-        if (Input.IsActionPressed("turnR"))
-        {
-            _animatedSprite.Play("turnR");
-            _angDelta++;
-            _inputReceived = true;
-        }
-        Rotation += _angDelta * _AngularAccleration;
+		if (Input.IsActionPressed("turnR"))
+		{
+			_animatedSprite.Play("turnR");
+			_angDelta++;
+			_inputReceived = true;
+		}
+		Rotation += _angDelta * _AngularAccleration;
 
 		if(!_inputReceived)
 			_animatedSprite.Play("default");
